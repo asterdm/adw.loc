ebb43e79497ac0a4b9cdab6bd9d33c1ee3908af7
yugin <>
2017-10-01 13:38:36 +0300

    Initial commit

--- config/web.php
+++ config/web.php
@@ -61,7 +61,7 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-  /*   // configuration adjustments for 'dev' environment
+    // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
@@ -74,7 +74,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
-    ]; */
+    ]; 
 }
 
 return $config;
